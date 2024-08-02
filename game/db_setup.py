af5a44dbd8ceb402c6bc86a0f38e5d7e04a8c3b1
Charlie <ceadorney@gmail.com>
2024-08-02 00:19:44 -0400

    db initialized using postgre

--- game/db_setup.py
+++ game/db_setup.py
@@ -1,36 +1,40 @@
-import sqlite3
+import psycopg2
+import os
 
-def init_db():
-    with sqlite3.connect('sessions.db') as conn:
-        cursor = conn.cursor()
+def init_db(database_url):
+    conn = psycopg2.connect(database_url, sslmode='require')
+    with conn.cursor() as cursor:
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS active_sessions (
-                session_id TEXT PRIMARY KEY,
-                start_time TEXT,
-                time REAL,
-                pauses TEXT,
-                pause_start TEXT,
-                clue1 TEXT,
-                clue2 TEXT,
-                answer1 TEXT,
-                inbetween TEXT,
-                answer2 TEXT,
-                correct BOOLEAN
+            session_id TEXT PRIMARY KEY,
+            start_time TIMESTAMP,
+            time FLOAT,
+            pauses JSONB,
+            pause_start TIMESTAMP,
+            clue1 TEXT,
+            clue2 TEXT,
+            answer1 TEXT,
+            inbetween TEXT,
+            answer2 TEXT,
+            correct BOOLEAN,
+            response BOOLEAN[]
             )
         ''')
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS answers (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id SERIAL PRIMARY KEY,
                 answer1 TEXT,
                 in_between TEXT,
                 answer2 TEXT,
                 clue1 TEXT,
                 clue2 TEXT,
                 count1 INTEGER,
-                count2 INTEGER
+                count2 INTEGER,
+                date TIMESTAMP
             )
         ''')
-        conn.commit()
+    conn.commit()
+    conn.close()
 
 if __name__ == "__main__":
     init_db()
