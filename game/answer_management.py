e16461d740a871b428f25bb9a7dea1b15ab99404
Charlie <ceadorney@gmail.com>
2025-05-25 17:51:16 -0400

    added system print for archive updates

--- game/answer_management.py
+++ game/answer_management.py
@@ -78,6 +78,7 @@ def update_answers():
                             ''', (data[1], data[2], data[3], data[4], data[5], data[6], data[7]))
                 conn.commit()
                 print("Answers updated for " + str(now))
+                print("New clue added to archive.")
         else:
             clear_answers(conn)
             with conn.cursor() as cursor:
