acebfd57fa86b1d149494ef5b148a43853285962
Charlie <ceadorney@gmail.com>
2025-06-14 21:32:46 -0400

    connection pool in daily script closes

--- daily.py
+++ daily.py
@@ -1,4 +1,4 @@
-from game.db_setup import setup_db_pool
+from game.db_setup import setup_db_pool, connection_pool
 from game.session_management import clear_all_sessions
 from game.answer_management import update_answers
 from game.archive_management import filter_old_users
@@ -8,3 +8,4 @@ if __name__ == "__main__":
     clear_all_sessions()
     update_answers()
     filter_old_users()
+    connection_pool.closeall()
