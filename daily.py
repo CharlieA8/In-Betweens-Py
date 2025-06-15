7c2c80c2e57a4c9b9580fa042cdd3fd54880e9af
Charlie <ceadorney@gmail.com>
2025-06-14 21:24:54 -0400

    DB pool correctly setup in daily.py

--- daily.py
+++ daily.py
@@ -1,8 +1,10 @@
+from game.db_setup import setup_db_pool
 from game.session_management import clear_all_sessions
 from game.answer_management import update_answers
 from game.archive_management import filter_old_users
 
 if __name__ == "__main__":
+    setup_db_pool()
     clear_all_sessions()
     update_answers()
     filter_old_users()
